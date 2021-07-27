2faf5403ec8c3c8e817f909ea74780489dfe8f1d
Tamas Csontos <csontos.t77@gmail.com>
2021-07-27 23:42:26 +0200

    basic rule test setup

---
+++ client/test/rules/helpers.js
@@ -0,0 +1,43 @@
+const firebase = require('@firebase/rules-unit-testing');
+const { loadFirestoreRules, initializeTestApp, clearFirestoreData } = firebase;
+const { readFileSync } = require('fs');
+const path = require('path');
+
+module.exports.setup = async (auth, data) => {
+  const projectId = 'test-app';
+  const app = initializeTestApp({
+    projectId,
+    auth
+  });
+
+  const db = app.firestore();
+
+  // Write mock documents before rules
+  if (data) {
+    // const admin = initializeAdminApp({
+    //   projectId
+    // });
+
+    for (const key in data) {
+      const ref = db.doc(key);
+      await ref.set(data[key]);
+    }
+  }
+
+  // Apply rules
+  await loadFirestoreRules({
+    projectId,
+    rules: readFileSync(path.resolve('../firestore.rules'), 'utf8')
+  });
+
+  return db;
+};
+
+module.exports.teardown = async () => {
+  await Promise.all(
+    firebase.apps().map((app) => {
+      app.delete();
+    })
+  );
+  await clearFirestoreData({ projectId: 'test-app' });
+};
---
+++ client/test/rules/messages.spec.js
@@ -0,0 +1,43 @@
+const { assertFails, assertSucceeds } = require('@firebase/rules-unit-testing');
+const { setup, teardown } = require('./helpers');
+
+const mockUser = {
+  uid: 'tester'
+};
+
+const mockData = {
+  'messages/message-1': {
+    message: 'Test message'
+  },
+  'messages/message-2': {
+    message: 'Test message'
+  }
+};
+
+describe('Message collection rules', () => {
+  let db;
+
+  beforeAll(async () => {
+    db = await setup(mockUser, mockData);
+  });
+
+  afterAll(async () => {
+    await teardown();
+  });
+
+  it('should ALLOW logged in users to submit messages', async () => {
+    const messagesRef = db.collection('messages');
+
+    await assertSucceeds(
+      messagesRef.add({
+        message: 'Added message'
+      })
+    );
+  });
+
+  it('should ALLOW logged in users to query messages', async () => {
+    const messagesRef = db.collection('messages');
+
+    await assertSucceeds(messagesRef.get());
+  });
+});
