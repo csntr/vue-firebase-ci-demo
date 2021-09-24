0ae4c249f677198a39ce2f39fbca583fe3a61137
Tamas Csontos <csontos.t77@gmail.com>
2021-09-24 10:50:28 +0200

    add failing cypress test for artifact testing

--- client/cypress/integration/increment.spec.js
+++ client/cypress/integration/increment.spec.js
@@ -6,4 +6,9 @@ describe('Increment test', () => {
   it('starts from zero', () => {
     cy.get('pre').should('have.text', 0)
   })
+
+  it('increments by 2', () => {
+    cy.get('button').click()
+    cy.get('pre').should('have.text', 2)
+  })
 })
